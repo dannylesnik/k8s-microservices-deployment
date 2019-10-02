26b593e283dee1d593b1c0593e29df438058b87a
Danny Lesnik <40953798+dannylesnik@users.noreply.github.com>
2019-10-03 00:19:28 +0300

    Update DockerSettings.scala

--- project/DockerSettings.scala
+++ project/DockerSettings.scala
@@ -13,7 +13,7 @@ object DockerSettings {
     dockerChmodType in Docker := DockerChmodType.UserGroupWriteExecute,
     dockerChmodType := DockerChmodType.UserGroupWriteExecute,
     daemonGroup := "root",
-    dockerRepository in Docker := Some("113379206287.dkr.ecr.us-east-1.amazonaws.com/development/codebuild-poc/"),
+    dockerRepository in Docker := Some("113379206287.dkr.ecr.us-east-1.amazonaws.com/development/codebuild-poc"),
     dockerBaseImage :=  "openjdk:11.0.3-jdk-slim-stretch"
   )
 }
