733f14822908cb3b4dd763155108a9b6e6b0f1f7
Danny Lesnik <40953798+dannylesnik@users.noreply.github.com>
2019-10-03 00:39:58 +0300

    Update DockerSettings.scala

--- project/DockerSettings.scala
+++ project/DockerSettings.scala
@@ -1,8 +1,8 @@
+import com.typesafe.sbt.packager.Keys.packageName
+import com.typesafe.sbt.packager.docker.DockerChmodType
 import com.typesafe.sbt.packager.docker.DockerPlugin.autoImport.{dockerBuildOptions, _}
 import com.typesafe.sbt.packager.linux.LinuxPlugin.autoImport.{daemonGroup, daemonUser}
-import sbt.Def
-import sbt.Keys.version
-import com.typesafe.sbt.packager.docker.DockerChmodType
+import sbt.Keys.{name, version}
 
 object DockerSettings {
 
@@ -14,6 +14,7 @@ object DockerSettings {
     dockerChmodType := DockerChmodType.UserGroupWriteExecute,
     daemonGroup := "root",
     dockerRepository in Docker := Some("113379206287.dkr.ecr.us-east-1.amazonaws.com/development/codebuild-poc"),
+    packageName in Docker := name.value,
     dockerBaseImage :=  "openjdk:11.0.3-jdk-slim-stretch"
   )
 }
